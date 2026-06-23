16c83080caec3c146a20a9fcff35998d839a03c0
abhay737 <122343672+abhay737@users.noreply.github.com>
2026-06-23 21:26:17 +0530

    Enhancements

--- camera-coverage-checker.py
+++ camera-coverage-checker.py
@@ -1,38 +1,49 @@
 def is_camera_set_sufficient(required_distance, required_light, cameras):
+
     min_distance, max_distance = required_distance
     min_light, max_light = required_light
 
-    distance_points = [min_distance, max_distance]
-    light_points = [min_light, max_light]
+    distance_boundaries = {min_distance, max_distance}
+    light_boundaries = {min_light, max_light}
 
     for camera in cameras:
-        distance_points.extend(camera['distance'])
-        light_points.extend(camera['light'])
+        distance_boundaries.update(camera["distance"])
+        light_boundaries.update(camera["light"])
 
-    distance_points = sorted(set(distance_points))
-    light_points = sorted(set(light_points))
+    distance_boundaries = sorted(distance_boundaries)
+    light_boundaries = sorted(light_boundaries)
 
-    for i in range(len(distance_points) - 1):
-        for j in range(len(light_points) - 1):
-            
-            test_distance = (distance_points[i] + distance_points[i + 1]) / 2
-            test_light = (light_points[j] + light_points[j + 1]) / 2
+    for i in range(len(distance_boundaries) - 1):
+        for j in range(len(light_boundaries) - 1):
 
-            if not(min_distance <= test_distance <= max_distance and min_light <= test_light <= max_light):
+            distance = (
+                distance_boundaries[i]
+                + distance_boundaries[i + 1]
+            ) / 2
+
+            light = (
+                light_boundaries[j]
+                + light_boundaries[j + 1]
+            ) / 2
+
+            if not (
+                min_distance <= distance <= max_distance
+                and
+                min_light <= light <= max_light
+            ):
                 continue
-            
-            covered = False
 
-            for camera in cameras:
-                if(camera['distance'][0] <= test_distance <= camera['distance'][1] and camera['light'][0] <= test_light <= camera['light'][1]):
-                    covered = True
-                    break
-            
-            if not covered:
+            if not any(
+                camera["distance"][0] <= distance <= camera["distance"][1]
+                and
+                camera["light"][0] <= light <= camera["light"][1]
+                for camera in cameras
+            ):
                 return False
 
     return True
-    
+
+
 required_distance = (0, 100)         
 required_light = (0,100)
 cameras = [
